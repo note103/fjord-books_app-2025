52c2f6b7b355b612882abaf0fc5fcc9e9b679b9b
Hiroaki Kadomatsu <note103@gmail.com>
2025-09-10 01:00:48 +0900

    Userモデルのバリデーションにエラーメッセージを記載しない意図をコメントで付記

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
   validates :avatar,
             content_type: { in: %w[image/jpeg image/png image/gif] },
             size: { less_than: 5.megabytes }
+  # エラーメッセージは内容の精度とメンテナンス性を考慮してgemの翻訳を使用
+  # https://github.com/igorkasyanchuk/active_storage_validations/blob/master/config/locales/ja.yml
 end
