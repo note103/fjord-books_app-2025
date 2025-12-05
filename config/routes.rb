99a193c3de4f31dad612b43114be078dff3a6e0d
Hiroaki Kadomatsu <note103@gmail.com>
2025-12-05 23:48:53 +0900

    routes.rbにネストされたリソースとしてコメントを追加
    - BookおよびReportリソースに対して、コメントの作成と削除を可能にするネストされたルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :reports
+  resources :books do
+    resources :comments, only: [:create, :destroy]
+  end
+  resources :reports do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: %i(index show)
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
