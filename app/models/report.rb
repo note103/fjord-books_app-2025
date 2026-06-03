25b741345955ba7c794d74039499751afd659ace
Hiroaki Kadomatsu <note103@gmail.com>
2026-06-03 20:14:47 +0900

    元の方法に戻す

--- app/models/report.rb
+++ app/models/report.rb
@@ -11,15 +11,17 @@ class Report < ApplicationRecord
            foreign_key: 'mentioning_report_id',
            dependent: :destroy,
            inverse_of: :mentioning_report
-  has_many :mentioned_reports,
-           through: :sending_mentions
+  has_many :mentioning_reports,
+           through: :sending_mentions,
+           source: :mentioned_report
   has_many :receiving_mentions,
            class_name: 'ReportMention',
            foreign_key: 'mentioned_report_id',
            dependent: :destroy,
            inverse_of: :mentioned_report
-  has_many :mentioning_reports,
-           through: :receiving_mentions
+  has_many :mentioned_reports,
+           through: :receiving_mentions,
+           source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
